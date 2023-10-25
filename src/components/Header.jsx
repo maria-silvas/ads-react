57e5fdbdd2efa95c218f7f45d2d78b61c2f68bf3
maria-silvas <silvamariaeduarda17@gmail.com>
2023-10-25 19:35:09 -0300

    alteração

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
+import { Container, Image, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export default function Header() {
+export default function HeaderBootstrap() {
     return (
-        <header>
-            <nav className="navbar navbar-expand-lg">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
+        <Navbar>
+            <Container>
+                <Navbar.Brand>
+                    <Link to="/" className="nav-link" >
+                        <Image
+                            src={require("../assets/images/logosenac.png")}
+                            width={150}
+                        />
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navegacao" />
+                <Navbar.Collapse id="navegacao" className="justify-content-end">
+                    <Nav className="navbar-expand-lg navbar-light">
                         <Link to="/" className="nav-link">Inicial</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/sobre" className="nav-link">Sobre</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/contato" className="nav-link">Contato</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+                        <Link to="sobre" className="nav-link">Sobre</Link>
+                        <Link to="noticias" className="nav-link">Notícias</Link>
+                        <Link to="contato" className="nav-link">Contato</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import HomeScreen from './../screens/HomeScreen';
 import Body from './../components/Body';
 import SobreScreen from "../screens/SobreScreen";
 import ContatoScreen from "../screens/ContatoScreen";
-
+import NoticiasScreen from "../screens/NoticiasScreen";
+import { Container } from "react-bootstrap";
+import NoticiasSingleScreen from "../screens/NoticiasSingleScreen";
 /**
  * Retorna o componente de navegação
  * primária do meu aplicativo / site
  */
-export default function RoutesRoot() {
+export default function RootNavigation() {
     return (
         <BrowserRouter>
             {/* BrowserRouter é o elemento que define a navegação base */}
@@ -16,11 +18,35 @@ export default function RoutesRoot() {
                 {/* Routes é a definição de rotas necessário */}
                 <Route path="/" element={<Body />}>
                     {/* Aqui definimos as rotas */}
-                    <Route element={<HomeScreen />} path="/" ></Route>
-                    <Route element={<SobreScreen />} path="/sobre"></Route>
-                    <Route element={<ContatoScreen />} path="/contato"></Route>
+                    <Route
+                        path="/"
+                        element={<HomeScreen />} />
+                    <Route
+                        path="/sobre"
+                        element={<SobreScreen />} />
+                    <Route
+                        path="/noticias"
+                        element={<NoticiasScreen />} />
+                    <Route
+                        path="/noticias/:idNoticia"
+                        element={<NoticiasSingleScreen />} />
+                    <Route
+                        path="/contato"
+                        element={<ContatoScreen />} />
+                    <Route
+                        path="*"
+                        element={<Pagina404 />} />
                 </Route>
             </Routes>
         </BrowserRouter>
+    )
+}
+
+function Pagina404() {
+    return (
+        <Container>
+            <h1>404</h1>
+            <p>Página não encontrada</p>
+        </Container>
     )
 }
